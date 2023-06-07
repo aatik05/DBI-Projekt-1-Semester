5d53857560c10cd2c4867eeeb6a73d44b70aab8f
aatik05 <rah22182@spengergasse.at>
2023-06-07 10:18:20 +0200

    SQL Script

---
+++ create_db.sql
@@ -0,0 +1,46 @@
+USE tempdb;
+GO
+IF EXISTS(SELECT * FROM sys.databases WHERE [name] = N'Taskverwaltung')
+    BEGIN
+        -- Disconnect all users and recreate database.
+        ALTER DATABASE Taskverwaltung SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    	DROP DATABASE Taskverwaltung;
+    END;
+END;
+USE Taskverwaltung;   -- Change to your database name (USE does not allow variables)
+GO
+CREATE DATABASE Taskverwaltung
+GO
+USE Taskverwaltung
+GO
+
+CREATE TABLE Student (
+  StudentId INTEGER PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Task (
+  TaskId INTEGER PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Description VARCHAR(255) NOT NULL,
+  DueDate DATE NOT NULL,
+  StudentId INTEGER,
+  CONSTRAINT fk_Student_Task FOREIGN KEY (StudentId) REFERENCES Student(StudentId),
+  CONSTRAINT chk_Task_DueDate CHECK (DueDate <= '2023')
+);
+
+CREATE TABLE Professor (
+  ProfessorId INTEGER PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE TaskRating (
+  TaskId INTEGER NOT NULL,
+  Rating INTEGER NOT NULL,
+  ProfessorId INTEGER NOT NULL,
+  CONSTRAINT fk_TaskRating_Task FOREIGN KEY (TaskId) REFERENCES Task(TaskId),
+  CONSTRAINT fk_TaskRating_Professor FOREIGN KEY (ProfessorId) REFERENCES Professor(ProfessorId),
+  CONSTRAINT chk_TaskRating_Rating CHECK (Rating >= 1 AND Rating <= 5)
+);
